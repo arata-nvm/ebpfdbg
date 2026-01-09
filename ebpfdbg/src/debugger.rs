e12a59b7a5169088e8269103b766544cf45531ca
Arata <arata04962@gmail.com>
2026-01-09 13:33:51 +0900

    feat: impl HostIo

--- ebpfdbg/src/debugger.rs
+++ ebpfdbg/src/debugger.rs
@@ -1,4 +1,9 @@
-use std::{ffi::CString, io::IoSliceMut, path::Path};
+use std::{
+    ffi::CString,
+    io::IoSliceMut,
+    os::fd::{BorrowedFd, IntoRawFd},
+    path::Path,
+};
 
 use aya::{maps::HashMap, programs::UProbe};
 use ebpfdbg_common::RegisterState;
@@ -6,17 +11,30 @@ use gdbstub::{
     arch::Arch,
     target::{
         Target, TargetError, TargetResult,
-        ext::base::{
-            BaseOps,
-            single_register_access::{SingleRegisterAccess, SingleRegisterAccessOps},
-            singlethread::{SingleThreadBase, SingleThreadResume, SingleThreadResumeOps},
+        ext::{
+            base::{
+                BaseOps,
+                single_register_access::{SingleRegisterAccess, SingleRegisterAccessOps},
+                singlethread::{SingleThreadBase, SingleThreadResume, SingleThreadResumeOps},
+            },
+            extended_mode::{
+                Args, AttachKind, CurrentActivePid, CurrentActivePidOps, ExtendedMode,
+                ExtendedModeOps, ShouldTerminate,
+            },
+            host_io::{
+                HostIo, HostIoClose, HostIoCloseOps, HostIoErrno, HostIoError, HostIoOpen,
+                HostIoOpenFlags, HostIoOpenMode, HostIoOpenOps, HostIoOps, HostIoPread,
+                HostIoPreadOps, HostIoReadlink, HostIoReadlinkOps, HostIoResult,
+            },
         },
     },
 };
 use gdbstub_arch::x86::reg::id::{X86_64CoreRegId, X86SegmentRegId};
 use nix::{
+    fcntl::{self, OFlag},
     sys::{
         signal::{self, Signal},
+        stat::Mode,
         uio::{self, RemoteIoVec},
         wait::{self, WaitPidFlag, WaitStatus},
     },
@@ -116,6 +134,10 @@ impl Target for Debugger {
     }
 
     fn support_extended_mode(&mut self) -> Option<ExtendedModeOps<'_, Self>> {
+        Some(self)
+    }
+
+    fn support_host_io(&mut self) -> Option<HostIoOps<'_, Self>> {
         Some(self)
     }
 
@@ -289,3 +311,72 @@ impl CurrentActivePid for Debugger {
     }
 }
 
+impl HostIo for Debugger {
+    fn support_open(&mut self) -> Option<HostIoOpenOps<'_, Self>> {
+        Some(self)
+    }
+
+    fn support_readlink(&mut self) -> Option<HostIoReadlinkOps<'_, Self>> {
+        Some(self)
+    }
+
+    fn support_pread(&mut self) -> Option<HostIoPreadOps<'_, Self>> {
+        Some(self)
+    }
+
+    fn support_close(&mut self) -> Option<HostIoCloseOps<'_, Self>> {
+        Some(self)
+    }
+}
+
+impl HostIoOpen for Debugger {
+    fn open(
+        &mut self,
+        filename: &[u8],
+        flags: HostIoOpenFlags,
+        mode: HostIoOpenMode,
+    ) -> HostIoResult<u32, Self> {
+        let flags = OFlag::from_bits(flags.bits() as i32).expect("invalid flags");
+        let mode = Mode::from_bits(mode.bits() as u32).expect("invalid mode");
+        let fd =
+            fcntl::open(filename, flags, mode).map_err(|e| HostIoError::Errno(map_errno(e)))?;
+        let fd = fd.into_raw_fd();
+        Ok(fd as u32)
+    }
+}
+
+impl HostIoReadlink for Debugger {
+    fn readlink(&mut self, filename: &[u8], buf: &mut [u8]) -> HostIoResult<usize, Self> {
+        let path = fcntl::readlink(filename).map_err(|e| HostIoError::Errno(map_errno(e)))?;
+        let path_bytes = path.as_encoded_bytes();
+        let len = buf.len().min(path_bytes.len());
+        buf[..len].copy_from_slice(&path_bytes[..len]);
+        Ok(len)
+    }
+}
+
+impl HostIoPread for Debugger {
+    fn pread(
+        &mut self,
+        fd: u32,
+        count: usize,
+        offset: u64,
+        buf: &mut [u8],
+    ) -> HostIoResult<usize, Self> {
+        let fd = unsafe { BorrowedFd::borrow_raw(fd as i32) };
+        let nread = uio::pread(fd, &mut buf[..count], offset as i64)
+            .map_err(|e| HostIoError::Errno(map_errno(e)))?;
+        Ok(nread)
+    }
+}
+
+impl HostIoClose for Debugger {
+    fn close(&mut self, fd: u32) -> HostIoResult<(), Self> {
+        unistd::close(fd as i32).map_err(|e| HostIoError::Errno(map_errno(e)))?;
+        Ok(())
+    }
+}
+
+fn map_errno(e: nix::errno::Errno) -> HostIoErrno {
+    unsafe { std::mem::transmute::<_, HostIoErrno>(e as u16) }
+}
